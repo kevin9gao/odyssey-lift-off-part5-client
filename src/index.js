cd798b3f7b18fb4ac8a2bad9f7fc569a65ee3129
kevin <kevingao52997@gmail.com>
2023-02-24 18:47:06 -0800

    new server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-part5-server-production-bbd3.up.railway.app/", // change to YOUR own production server
+  uri: "https://odysseyliftoff-part5-server-new-production.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
   // name: "web",
   // version: "1.0",
